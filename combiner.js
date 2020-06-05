5b8ffb4ff3bb170ca4a9609dba864543ca5e4896
kaufmjoh <kaufmjoh@oregonstate.edu>
2020-06-05 13:20:08 -0700

    Added color combiner html

---
+++ combiner.js
@@ -0,0 +1,10 @@
+var inputs = document.getElementsByClassName("site-title");
+console.log(inputs.length);
+
+
+
+function updateColor()
+{
+  console.log("Here");
+  console.log(event.target.value);
+}
