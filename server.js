b0a98bd34822d3e9d54a00a2840daf9a191d87e5
Kyle <huangkyle110@gmail.com>
2020-06-09 19:45:52 -0700

    added color combiner to server

--- server.js
+++ server.js
@@ -6,16 +6,33 @@ var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+/*
+ * Default home page to load... index.html
+ */
 app.get('/', function(req, res, next) {
     // res.status(200);
     res.status(200).sendFile(__dirname + "/public/index.html");
 });
 
+/*
+ * page for pixel art
+ */
 app.get('/pixelArt.html', function(req, res, next) {
     // res.status(200);
-    res.status(200).sendFile(__dirname + "/public/index.html");
+    res.status(200).sendFile(__dirname + "/public/pixelArt.html");
 });
 
+/*
+ * page for color combiner
+ */
+app.get('/combiner.html', function(req, res, next) {
+    // res.status(200);
+    res.status(200).sendFile(__dirname + "/combiner.html");
+});
+
+/*
+ * 404 page
+ */
 app.get('*', function(req, res, next) {
     // res.status(200);
     res.status(404).sendFile(__dirname + "/public/404.html");
