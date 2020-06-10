55021a5647cb812e57a6468f88fdaa606e7da505
khuang110 <huangkyle110@gmail.com>
2020-06-10 12:59:54 -0700

    Updated

--- save.js
+++ save.js
@@ -1,31 +1,23 @@
 /*
  *   save image as a png
  */
-var saveBut = document.getElementById('download');
+var saveBut = document.getElementById('btn');
 
 saveBut.addEventListener('click', saveImage);
 
 function saveImage() {
-    console.log("------------------");
-    var data = canvas.toDataURL();
 
-    var req = new XMLHttpRequest();
+    canvas.toBlob(function(blob) {
+        var newImg = document.createElement('img'),
+            url = URL.createObjectURL(blob);
 
-    req.onreadystatechange = function() {
-        // check if fully loaded and page is ok
-        if (req.readyState == 4 && req.status == 200) {
-            var res = req.responseText;
-            window.open(data, '_blank', 'location=0, menubar=0');
-            console.log(res);
-        }
-    }
+        newImg.onload = function() {
+            // no longer need to read the blob so it's revoked
+            URL.revokeObjectURL(url);
+        };
 
-
-    // //send data
-    req.open('POST', 'img.php', true);
-    req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    req.send('img=' + data);
-
-    console.log("afeter");
+        newImg.src = url;
+        document.body.appendChild(newImg);
+    });
 
 }
